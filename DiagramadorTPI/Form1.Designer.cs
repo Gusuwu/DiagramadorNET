56bf9282b03ff91383840efa16ba316d00bd3da9
Gusuwu <augustoadrian@hotmail.com>
2022-07-01 03:02:50 -0300

    Proyecto subido

---
+++ DiagramadorTPI/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace DiagramadorTPI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirArchivoMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarArchivoMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.designer1 = new Dalssoft.DiagramNet.Designer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.abrirToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.guardarToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.borrarToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.agregarBoton = new System.Windows.Forms.ToolStripSplitButton();
+            this.rectanguloToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.circuloToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.unirToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.fondoToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.frenteToolStrip = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirArchivoMenu,
+            this.guardarArchivoMenu,
+            this.salirMenu});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirArchivoMenu
+            // 
+            this.abrirArchivoMenu.Name = "abrirArchivoMenu";
+            this.abrirArchivoMenu.Size = new System.Drawing.Size(180, 22);
+            this.abrirArchivoMenu.Text = "Abrir";
+            this.abrirArchivoMenu.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarArchivoMenu
+            // 
+            this.guardarArchivoMenu.Name = "guardarArchivoMenu";
+            this.guardarArchivoMenu.Size = new System.Drawing.Size(180, 22);
+            this.guardarArchivoMenu.Text = "Guardar";
+            this.guardarArchivoMenu.Click += new System.EventHandler(this.guardarArchivoMenu_Click);
+            // 
+            // salirMenu
+            // 
+            this.salirMenu.Name = "salirMenu";
+            this.salirMenu.Size = new System.Drawing.Size(180, 22);
+            this.salirMenu.Text = "Salir";
+            this.salirMenu.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // designer1
+            // 
+            this.designer1.AutoScroll = true;
+            this.designer1.BackColor = System.Drawing.SystemColors.Window;
+            this.designer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.designer1.Location = new System.Drawing.Point(0, 24);
+            this.designer1.Name = "designer1";
+            this.designer1.Size = new System.Drawing.Size(800, 426);
+            this.designer1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStrip,
+            this.guardarToolStrip,
+            this.toolStripSeparator2,
+            this.borrarToolStrip,
+            this.toolStripSeparator1,
+            this.agregarBoton,
+            this.unirToolStrip,
+            this.toolStripSeparator3,
+            this.fondoToolStrip,
+            this.frenteToolStrip});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // abrirToolStrip
+            // 
+            this.abrirToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.abrirToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("abrirToolStrip.Image")));
+            this.abrirToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.abrirToolStrip.Name = "abrirToolStrip";
+            this.abrirToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.abrirToolStrip.Text = "Abrir";
+            this.abrirToolStrip.Click += new System.EventHandler(this.abrirToolStrip_Click_1);
+            // 
+            // guardarToolStrip
+            // 
+            this.guardarToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.guardarToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("guardarToolStrip.Image")));
+            this.guardarToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.guardarToolStrip.Name = "guardarToolStrip";
+            this.guardarToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.guardarToolStrip.Text = "Guardar";
+            this.guardarToolStrip.Click += new System.EventHandler(this.guardarToolStrip_Click_1);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // borrarToolStrip
+            // 
+            this.borrarToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.borrarToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("borrarToolStrip.Image")));
+            this.borrarToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.borrarToolStrip.Name = "borrarToolStrip";
+            this.borrarToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.borrarToolStrip.Text = "Borrar";
+            this.borrarToolStrip.Click += new System.EventHandler(this.borrarToolStrip_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // agregarBoton
+            // 
+            this.agregarBoton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.agregarBoton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectanguloToolStrip,
+            this.circuloToolStrip});
+            this.agregarBoton.Image = ((System.Drawing.Image)(resources.GetObject("agregarBoton.Image")));
+            this.agregarBoton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.agregarBoton.Name = "agregarBoton";
+            this.agregarBoton.Size = new System.Drawing.Size(32, 22);
+            this.agregarBoton.Text = "Agregar Figura";
+            // 
+            // rectanguloToolStrip
+            // 
+            this.rectanguloToolStrip.Name = "rectanguloToolStrip";
+            this.rectanguloToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.rectanguloToolStrip.Text = "Rectangulo";
+            this.rectanguloToolStrip.Click += new System.EventHandler(this.rectanguloToolStrip_Click);
+            // 
+            // circuloToolStrip
+            // 
+            this.circuloToolStrip.Name = "circuloToolStrip";
+            this.circuloToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.circuloToolStrip.Text = "Circulo";
+            this.circuloToolStrip.Click += new System.EventHandler(this.circuloToolStrip_Click);
+            // 
+            // unirToolStrip
+            // 
+            this.unirToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.unirToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("unirToolStrip.Image")));
+            this.unirToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.unirToolStrip.Name = "unirToolStrip";
+            this.unirToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.unirToolStrip.Text = "Unir";
+            this.unirToolStrip.Click += new System.EventHandler(this.unirToolStrip_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // fondoToolStrip
+            // 
+            this.fondoToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.fondoToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("fondoToolStrip.Image")));
+            this.fondoToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fondoToolStrip.Name = "fondoToolStrip";
+            this.fondoToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.fondoToolStrip.Text = "Deshacer";
+            this.fondoToolStrip.Click += new System.EventHandler(this.fondoToolStrip_Click);
+            // 
+            // frenteToolStrip
+            // 
+            this.frenteToolStrip.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.frenteToolStrip.Image = ((System.Drawing.Image)(resources.GetObject("frenteToolStrip.Image")));
+            this.frenteToolStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.frenteToolStrip.Name = "frenteToolStrip";
+            this.frenteToolStrip.Size = new System.Drawing.Size(23, 22);
+            this.frenteToolStrip.Text = "Rehacer";
+            this.frenteToolStrip.Click += new System.EventHandler(this.frenteToolStrip_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.designer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirArchivoMenu;
+        private System.Windows.Forms.ToolStripMenuItem guardarArchivoMenu;
+        private System.Windows.Forms.ToolStripMenuItem salirMenu;
+        private Dalssoft.DiagramNet.Designer designer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton abrirToolStrip;
+        private System.Windows.Forms.ToolStripButton guardarToolStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton borrarToolStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSplitButton agregarBoton;
+        private System.Windows.Forms.ToolStripMenuItem rectanguloToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem circuloToolStrip;
+        private System.Windows.Forms.ToolStripButton unirToolStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton fondoToolStrip;
+        private System.Windows.Forms.ToolStripButton frenteToolStrip;
+    }
+}
+
---
+++ DiagramadorTPI/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dalssoft.DiagramNet;
+
+namespace DiagramadorTPI
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        private bool cambioDesigner = true;
+        public string FileName { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void abrirArchivo() { 
+            openFileDialog1.FileName = FileName;
+            openFileDialog1.Filter = "JSON(*.json)|*.json|EXCEL(*.xlsx)|*.xlsx|Todos los archivos(*.*)|*.*";
+            openFileDialog1.DefaultExt = ".json";
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                FileName = openFileDialog1.FileName;
+
+                designer1.Open(openFileDialog1.FileName);
+            }
+        }
+
+        public void guardarArchivo() { 
+            saveFileDialog1.FileName = FileName;
+            saveFileDialog1.Filter = "JSON(*.json)|*.json|EXCEL(*.xlsx)|*.xlsx|Todos los archivos(*.*)|*.*";
+            saveFileDialog1.DefaultExt = ".json";
+            if (saveFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                FileName = saveFileDialog1.FileName;
+                designer1.Save(saveFileDialog1.FileName);
+                Bitmap bmp = new Bitmap(designer1.Width, designer1.Height);
+                designer1.DrawToBitmap(bmp, new Rectangle(0, 0, designer1.Width, designer1.Height));
+                bmp.Save(saveFileDialog1.FileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+        public void agregar(ElementType e) {
+            switch (e)
+            {
+                case ElementType.Rectangle:
+
+                    rectanguloToolStrip.Checked = true;
+                    break;
+                case ElementType.Elipse:
+
+                    circuloToolStrip.Checked = true;
+                    break;
+            }
+
+            if (cambioDesigner)
+            {
+                designer1.Document.Action = DesignerAction.Add;
+                designer1.Document.ElementType = e;
+            }
+        }
+        public void borrar() {
+            if (cambioDesigner)
+                designer1.Document.DeleteSelectedElements();
+        }
+
+        public void conectar() {
+            if (cambioDesigner)
+                designer1.Document.Action = DesignerAction.Connect;
+        }
+
+        public void conector(LinkType lt) {
+            designer1.Document.LinkType = lt;
+            conectar();
+        }
+
+        private void deshacerRehacer()
+        {
+
+            fondoToolStrip.Enabled = designer1.CanUndo;
+
+            frenteToolStrip.Enabled = designer1.CanRedo;
+        }
+
+        private void deshacer()
+        {
+            if (designer1.CanUndo)
+                designer1.Undo();
+
+            deshacerRehacer();
+        }
+
+        private void rehacer()
+        {
+            if (designer1.CanRedo)
+                designer1.Redo();
+
+            deshacerRehacer();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            abrirArchivo();
+        }
+
+        private void guardarArchivoMenu_Click(object sender, EventArgs e)
+        {
+            guardarArchivo();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+               this.Close();
+        }
+
+        private void abrirToolStrip_Click_1(object sender, EventArgs e)
+        {
+            abrirArchivo();
+        }
+
+        private void guardarToolStrip_Click_1(object sender, EventArgs e)
+        {
+            guardarArchivo();
+        }
+
+        private void cortarToolStrip_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fondoToolStrip_Click(object sender, EventArgs e)
+        {
+            deshacer();
+        }
+
+        private void frenteToolStrip_Click(object sender, EventArgs e)
+        {
+            rehacer();
+        }
+
+        private void rectanguloToolStrip_Click(object sender, EventArgs e)
+        {
+            agregar(ElementType.RectangleNode);
+        }
+
+        private void circuloToolStrip_Click(object sender, EventArgs e)
+        {
+            agregar(ElementType.ElipseNode);
+        }
+
+        private void borrarToolStrip_Click(object sender, EventArgs e)
+        {
+            borrar();
+        }
+
+        private void unirToolStrip_Click(object sender, EventArgs e)
+        {
+            conector(LinkType.Straight);
+        }
+    }
+}
